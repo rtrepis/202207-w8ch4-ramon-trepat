383199f32440b8802972fb80d8797ecf5ba516c5
Ramon Trepat <r.trepis@gmail.com>
2022-08-25 18:38:35 +0200

    Added create robot debbug

---
+++ src/createRobot.ts
@@ -0,0 +1,19 @@
+import { type } from "os";
+
+interface Robot {
+  name: string;
+  type: "Cleaner" | "Waiter" | "Developer";
+  battery: number;
+}
+
+const createRobot = () => {
+  const robot: Robot = {
+    name: "tre234",
+    type: "Cleaner",
+    battery: 100,
+  };
+
+  return robot;
+};
+
+export default createRobot;
